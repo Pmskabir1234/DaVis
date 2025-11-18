161a67af9de8f6ce6ea19aa780d2f10d86113e40
Kabir <pmskabir123@gmail.com>
2025-11-18 23:00:32 +0530

    markdown changes

--- app.py
+++ app.py
@@ -4,13 +4,23 @@ import matplotlib.pyplot as plt
 
 st.set_page_config(page_title='DaVis', page_icon='📊', layout='centered')
 
-st.title("Welcome to DaVis")
+st.markdown(
+    "<h1>"
+    "<span style='color:blue'>Wel</span>"
+    "<span style='color:cyan'>Come</span>"
+    " to "
+    "<span style='color:red'>Da</span>"
+    "<span style='color:pink'>Vis</span>"
+    "</h1>",
+    unsafe_allow_html= True
+)
 st.header("Your data decoder,visualizer buddy")
 
 st.divider()
 st.write("\n")
 
-loaded_file = st.file_uploader("Upload your csv file",type=['csv'])
+loaded_file = st.file_uploader("Upload your csv file",type=(['csv']))
+
 if loaded_file is not None:
     df = pd.read_csv(loaded_file)
     if st.button("Click here to see tabular preview"):
@@ -18,11 +28,11 @@ if loaded_file is not None:
 
     st.write("Visualising Options")
     column = st.selectbox("Choose a column:",df.columns.unique())
-    fig, axs = plt.subplots()
-    axs.bar(df.index,df[column])
+    fig, axs = plt.subplots()  
+    axs.plot(df.index,df[column])
     axs.set_title(f'Visuaizing {column}')
     axs.set_ylabel(f'{column}')
-    axs.set_xlabel(f'Index')
+    axs.set_xlabel(f'Index')   
     st.pyplot(fig)
 
 
