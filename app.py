77cadcdffb2540b732020de5ced8c0c49aeb17a0
Kabir <pmskabir123@gmail.com>
2025-11-20 20:33:30 +0530

    customisation
    added few customization options in file and graph type

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import copy
+import openpyxl
 
 st.set_page_config(page_title='DaVis', page_icon='📊', layout='centered')
 
@@ -20,23 +21,30 @@ st.header("Your data decoder,visualizer buddy")
 st.divider()
 st.write("\n")
 
-loaded_file = st.file_uploader("Upload your csv file",type=(['csv']))
+type_of = st.selectbox("Select the type of your file",['csv','xls','xlsx'])
+loaded_file = st.file_uploader("Upload your file",type=([type_of]))
 
 if loaded_file is not None:
 
     #dataframe reading
-    df = pd.read_csv(loaded_file)
+    if type_of == 'csv':
+        df = pd.read_csv(loaded_file)
+    else:
+        df = pd.read_excel(loaded_file)
     if st.button("Click here to see tabular preview"):
         st.dataframe(df)
 
     #data visualisation part
-    st.write("Visualising Options")
-    column = st.selectbox("Choose a column:",df.columns.unique())
+    st.write("Visualizing Options")
+    column = st.selectbox("Choose a column:",[col for col in df.columns if pd.api.types.is_numeric_dtype(df[col])])
+    graph_type = st.radio("Plot type",['Plot','Bar','Scatter'])
+    plt.style.use('ggplot')  
     fig, axs = plt.subplots()  
-    axs.plot(df.index,df[column])
-    axs.set_title(f'Visuaizing {column}')
+    plot_func = getattr(axs, graph_type.lower())
+    plot_func(df.index,df[column])
+    axs.set_title(f'Visualizing {column}')
     axs.set_ylabel(f'{column}')
-    axs.set_xlabel(f'Index')   
+    axs.set_xlabel(f'Index') 
     st.pyplot(fig)
 
     #sidebar and filter options
@@ -70,6 +78,28 @@ if loaded_file is not None:
     st.dataframe(filter_dataframe(df))
 
 
+st.markdown(
+    """
+    <style>
+    .footer {
+        position: fixed;
+        left: 0;
+        bottom: 0;
+        width: 100%;
+        text-align: center;
+        padding: 8px;
+        color: #6c6c6c;
+        font-size: 14px;
+        font-family: -apple-system, BlinkMacSystemFont, sans-serif;
+    }
+    </style>
+
+    <div class="footer">
+        Built with curiosity and passion @SaadKabir
+    </div>
+    """,
+    unsafe_allow_html=True
+)
 
 
 
